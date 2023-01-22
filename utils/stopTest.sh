f5f73c2ac69554d49202ba25eb320459aa611b3a
claude-at-748293476463 <claude.ducharme@gmail.com>
2023-01-23 04:50:18 +0800

    Enhance test script

--- utils/stopTest.sh
+++ utils/stopTest.sh
@@ -1,6 +1,10 @@
+#!/bin/sh
 
-docker stop `docker ps | grep "redislabs/redisearch:latest" | head -n 1 | tr -s ' ' | cut -d " " -f 1`
+CONTAINER_ID=$(docker ps | grep "redislabs/redisearch:latest" | awk '{print $1}')
+if [ ! -z "$CONTAINER_ID" ]; then docker stop $CONTAINER_ID; fi
 
-kill `ps aux --sort=-%mem | grep "node utils/streamDevLocToRedis.js" | head -n 1 | tr -s ' ' | cut -d " " -f 2`
+PROC_ID=$(ps -ef | grep "node" | grep "utils/streamDevLocToRedis.js" | awk '{print $2}')
+if [ ! -z "$PROC_ID" ]; then kill $PROC_ID; fi
 
-kill `ps aux --sort=-%mem | grep "node dist/main.js" | head -n 1 | tr -s ' ' | cut -d " " -f 2`
+PROC_ID=$(ps -ef | grep "node" | grep "utils/main.js" | awk '{print $2}')
+if [ ! -z "$PROC_ID" ]; then kill $PROC_ID; fi
